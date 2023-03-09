c64b72ccb316e0faf22a3a8983b3de907015c235
amdiggs <amdiggs@ucdavis.edu>
2023-03-09 15:17:47 -0800

    Initial Commit

---
+++ VolumeData/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VolumeData
+//
+//  Created by Andrew Diggs on 3/9/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
